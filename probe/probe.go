23dfb1e49277c634b755bf1db537c6629753ffb4
Michael Fox <mfox@amplify.security>
2022-05-16 15:12:42 -0500

    probe commit

--- probe/probe.go
+++ probe/probe.go
@@ -2,6 +2,7 @@ package probe
 
 import (
 	"context"
+
 	"github.com/rs/zerolog"
 )
 
@@ -14,22 +15,21 @@ type (
 		log     *zerolog.Logger
 		ctx     context.Context
 		cancel  context.CancelFunc
-		work    <-chan Runner
+		work    chan Runner
 		done    chan struct{}
 		working bool
 	}
 )
 
 // NewProbe initializes and returns a new Probe.
-func NewProbe(ctx context.Context, work <-chan Runner, log *zerolog.Logger) *Probe {
-	childCtx, cancel := context.WithCancel(ctx)
-	return &Probe{
-		log:    log,
-		ctx:    childCtx,
-		cancel: cancel,
-		work:   work,
-		done:   make(chan struct{}),
+func NewProbe(ctx context.Context, work chan Runner, log *zerolog.Logger) *Probe {
+	p := &Probe{
+		log:  log,
+		ctx:  ctx,
+		work: work,
 	}
+	p.Work()
+	return p
 }
 
 // Working returns the status of the Probe.
@@ -39,7 +39,13 @@ func (p *Probe) Working() bool {
 
 // Work is the main event loop for the Probe. Work will start a new goroutine.
 func (p *Probe) Work() {
+	if p.Working() {
+		return
+	}
+	// create a new cancelable child context only to be used by this goroutine
+	p.ctx, p.cancel = context.WithCancel(p.ctx)
 	p.working = true
+	p.done = make(chan struct{})
 	go func() {
 		for {
 			select {
@@ -55,10 +61,14 @@ func (p *Probe) Work() {
 	}()
 }
 
-// Stop will stop the Probe from doing further work. Stop blocks if wait is true until all work is complete.
+// Stop will stop the Probe from doing further work. Stop blocks if wait is true until current work is complete.
 func (p *Probe) Stop(wait bool) {
+	if !p.Working() {
+		return
+	}
 	p.cancel()
 	if wait {
 		<-p.done
+		return
 	}
 }
--- probe/probe_test.go
+++ probe/probe_test.go
@@ -10,13 +10,54 @@ import (
 )
 
 func TestProbe_Working(t *testing.T) {
+	var test bool
+	runner := func() {
+		test = true
+	}
 	log := zerolog.New(os.Stdout)
 	ctx := context.Background()
 	w := make(chan Runner)
 	p := NewProbe(ctx, w, &log)
-	assert.False(t, p.Working(), "p.Working == false")
-	p.Work()
-	assert.True(t, p.Working(), "p.Working == true")
+	assert.True(t, p.Working(), "NewProbe -> p.Working == true")
+	w <- runner
 	p.Stop(true)
-	assert.False(t, p.Working(), "p.Working == false")
+	assert.False(t, p.Working(), "p.Stop -> p.Working == false")
+	assert.True(t, test, "test == true")
+}
+
+func TestProbe_Stop(t *testing.T) {
+	log := zerolog.New(os.Stdout)
+	ctx := context.Background()
+	w := make(chan Runner)
+	done := make(chan struct{})
+	runner := func() {
+		<-done
+	}
+	p := NewProbe(ctx, w, &log)
+	assert.True(t, p.Working(), "NewProbe -> p.Working == true")
+	p.Stop(true)
+	assert.False(t, p.Working(), "p.Stop(true) -> p.Working == false")
+	p.Work()
+	assert.True(t, p.Working(), "p.Work -> p.Working == true")
+	w <- runner
+	p.Stop(false)
+	assert.True(t, p.Working(), "p.Stop(false) -> p.Working == true")
+	close(done)
+	p.Stop(true)
+	assert.False(t, p.Working(), "p.Stop(false) -> p.Working == false")
+	p.Stop(false)
+}
+
+func TestProbe_Work(t *testing.T) {
+	log := zerolog.New(os.Stdout)
+	ctx := context.Background()
+	w := make(chan Runner)
+	p := NewProbe(ctx, w, &log)
+	old := p.done
+	p.Work()
+	assert.Equal(t, old, p.done, "p.Work && p.Working -> old == new")
+	p.Stop(true)
+	p.Work()
+	assert.NotEqual(t, old, p.done, "p.Work && !p.Working -> old != new")
+	p.Stop(true)
 }

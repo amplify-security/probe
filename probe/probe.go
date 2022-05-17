12d8c57ab053e0c87ac070429a487788cd642e01
Michael Fox <mfox@amplify.security>
2022-05-17 10:59:16 -0500

    unique probe id and tests/benchmarks

--- probe/probe.go
+++ probe/probe.go
@@ -2,6 +2,10 @@ package probe
 
 import (
 	"context"
+	"encoding/hex"
+	"io"
+	"math/rand"
+	"time"
 
 	"github.com/rs/zerolog"
 )
@@ -19,23 +23,52 @@ type (
 		work     chan Runner
 		done     chan struct{}
 		working  bool
+		idle     bool
+		id       string
 	}
 )
 
+var (
+	randStream io.Reader = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+// getID returns a random hex string to be used as a unique identifier for a Probe.
+func getID(log *zerolog.Logger) string {
+	buf := make([]byte, 6)
+	if _, err := randStream.Read(buf); err != nil {
+		log.Panic().Err(err).Msg("unable to generate Probe ID")
+	}
+	return hex.EncodeToString(buf)[:6]
+}
+
 // NewProbe initializes and returns a new Probe.
 func NewProbe(ctx context.Context, work chan Runner, log *zerolog.Logger) *Probe {
+	id := getID(log)
+	ctxLogger := log.With().Str("probe_id", id).Logger()
 	p := &Probe{
-		log:  log,
+		log:  &ctxLogger,
 		ctx:  ctx,
 		work: work,
+		idle: true,
+		id:   id,
 	}
 	p.Work()
 	return p
 }
 
-// Working returns the status of the Probe.
+// ID returns the unique identifier of the Probe.
+func (p *Probe) ID() string {
+	return p.id
+}
+
+// Working returns the status of the Probe: true if work event loop is running.
 func (p *Probe) Working() bool {
 	return p.working
+}
+
+// Idle returns the status of the Probe: true if the Probe is Working but has no current work to execute.
+func (p *Probe) Idle() bool {
+	return p.idle
 }
 
 // Work is the main event loop for the Probe. Work will start a new goroutine.
@@ -48,15 +81,19 @@ func (p *Probe) Work() {
 	p.working = true
 	p.done = make(chan struct{})
 	go func() {
+		p.log.Debug().Msg("probe starting work event loop")
 		for {
 			select {
 			case <-p.childCtx.Done():
 				// the context is done, exit
+				p.log.Debug().Msg("probe shutting down")
 				p.working = false
 				close(p.done)
 				return
 			case runner := <-p.work:
+				p.idle = false
 				runner()
+				p.idle = true
 			}
 		}
 	}()
--- probe/probe_test.go
+++ probe/probe_test.go
@@ -2,12 +2,35 @@ package probe
 
 import (
 	"context"
-	"github.com/rs/zerolog"
-	"github.com/stretchr/testify/assert"
+	"errors"
+	"math/rand"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type (
+	// MockRandom is a mock Random stream for testing.
+	MockRandom struct {
+		mock.Mock
+	}
+)
+
+// Read implementation of io.Reader for MockRandom.
+func (m *MockRandom) Read(p []byte) (n int, err error) {
+	args := m.Called(p)
+	for _, i := range p {
+		p[i] = 0
+	}
+	if args.Get(1) != nil {
+		err = args.Error(1)
+	}
+	return args.Int(0), err
+}
 
 func TestProbe_Working(t *testing.T) {
 	var test bool
@@ -33,6 +56,38 @@ func TestProbe_Working(t *testing.T) {
 		time.Sleep(10 * time.Millisecond)
 	}
 	assert.False(t, p.Working(), "ctx cancel -> p.Working == false")
+}
+
+func TestProbe_Idle(t *testing.T) {
+	log := zerolog.New(os.Stdout)
+	ctx := context.Background()
+	w := make(chan Runner)
+	done := make(chan struct{})
+	runner := func() {
+		<-done
+	}
+	p := NewProbe(ctx, w, &log)
+	assert.True(t, p.Idle(), "NewProbe -> p.Idle == true")
+	w <- runner
+	assert.False(t, p.Idle(), "runner -> p.Idle == false")
+	close(done)
+	for i := 0; i < 10; i++ {
+		// wait for goroutine to become idle after finishing work
+		if p.Idle() {
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	assert.True(t, p.Idle(), "close(done) -> p.Idle == true")
+	p.Stop(true)
+}
+
+func TestProbe_ID(t *testing.T) {
+	log := zerolog.New(os.Stdout)
+	ctx := context.Background()
+	w := make(chan Runner)
+	p := NewProbe(ctx, w, &log)
+	assert.NotEmpty(t, p.ID(), "p.ID -> !empty")
 }
 
 func TestProbe_Stop(t *testing.T) {
@@ -70,4 +125,29 @@ func TestProbe_Work(t *testing.T) {
 	p.Work()
 	assert.NotEqual(t, old, p.done, "p.Work && !p.Working -> old != new")
 	p.Stop(true)
+}
+
+func TestGetID(t *testing.T) {
+	log := zerolog.New(os.Stdout)
+	id := getID(&log)
+	assert.NotEmpty(t, id, "id -> !empty")
+	m := &MockRandom{}
+	m.On("Read", mock.Anything).Return(3, nil)
+	randStream = m
+	id = getID(&log)
+	assert.Equal(t, "000000", id, "id -> 000000")
+	m = &MockRandom{}
+	m.On("Read", mock.Anything).Return(0, errors.New("unexpected error"))
+	randStream = m
+	assert.Panics(t, func() {
+		id = getID(&log)
+	}, "getID -> panic")
+	randStream = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func BenchmarkGetID(b *testing.B) {
+	log := zerolog.New(os.Stdout)
+	for i := 0; i < b.N; i++ {
+		getID(&log)
+	}
 }
